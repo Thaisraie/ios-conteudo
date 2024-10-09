cc63b4c6b68744f44b2f08a4e9c33c9f9a8f4bbd
Thais Siqueira <thaisdivino21@gmail.com>
2024-10-09 11:52:46 -0300

    Aula 03.

---
+++ aulas-js/aula03/substring.js
@@ -0,0 +1,24 @@
+// let nome = "aluno IOS";
+// let nomeNovo = nome.substring(2);
+// let nomeSplit = nome.split(" ");
+// console.log(nome.length);
+// console.log(nomeNovo);
+// console.log(nomeNovo.length);
+// console.log(nomeSplit);
+// let sobreNome = nomeSplit[1];
+// console.log(sobreNome);
+
+// let frutas = ["Banana", "Morango", "Abacaxi", "Mamão", "Melancia"]
+
+// console.log(frutas);
+// console.log(frutas.length);
+// console.log(typeof frutas[4]);
+// console.log(typeof frutas);
+
+// let  pessoa = "Thais Raie";
+// let novaPessoa = pessoa.replace("Raie", "Siqueira");
+// console.log(pessoa);
+// console.log(novaPessoa);
+
+// let nomeTrin = nome.Trin()
+// console.log(nomeTrin);
---
+++ aulas-js/metodosString.js
@@ -0,0 +1,26 @@
+let nome = "Thais";
+let idade = 29;
+let matriculaAtiva = true;
+
+console.log(`===============`);
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof matriculaAtiva);
+console.log(`===============`);
+
+let res = idade >= 60 ? "Pode fazer o curso" : "Não pode fazer o curso";
+
+console.log(`A aluna ${res} `);
+
+let aluno = "sim"
+let matricula = aluno == "sim" ? "o aluno está matriculado" : "o aluno não está matriculado";
+
+console.log(matricula);
+
+let eu = "Thais";
+
+let novoNome = eu.charAt(4);
+
+console.log(eu);
+console.log(novoNome);
+console.log(eu.length);
